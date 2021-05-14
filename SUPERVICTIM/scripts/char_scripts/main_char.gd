b6a5a13eb5563cb4e454071e6a21127df302c668
Tyler Zonies <tylerzonies@gmail.com>
2021-05-14 17:32:07 -0600

    added some script to main char

--- SUPERVICTIM/scripts/char_scripts/main_char.gd
+++ SUPERVICTIM/scripts/char_scripts/main_char.gd
@@ -1,23 +1,34 @@
 extends KinematicBody2D
 var velocity = Vector2(0,0)
-var fall_speed = 500
+var fall_speed = 5
 var x_direction = 0
 var y_direction = 0
 
 const UP = Vector2(0,-1)
+const GROUND_SPEED = 10000
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
+	#apply_gravity(delta)
+	move_and_slide(velocity, UP)
 	
 
-func get_input():
+func get_input(delta):
 	var LEFT = Input.is_action_pressed('left')
 	var RIGHT = Input.is_action_pressed('right')
 	var UP = Input.is_action_pressed('up')
 	var DOWN = Input.is_action_pressed('down')
 	
-	print(Input.is_action_pressed("right"))
-	
 	x_direction = -int(LEFT) + int(RIGHT)
 	y_direction = -int(DOWN) + int(UP)
-	print(x_direction)
+	move(x_direction, delta)
+
+func apply_gravity(delta):
+	velocity.y += fall_speed * delta
+	
+
+func move(direction, delta):
+	velocity.x = direction * GROUND_SPEED * delta
+
+
+
