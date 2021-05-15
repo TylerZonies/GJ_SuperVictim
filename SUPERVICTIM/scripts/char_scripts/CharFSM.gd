7cc65b43479e2403d7bef045dbd9bd5ca7790e60
Tyler Zonies <tylerzonies@gmail.com>
2021-05-14 19:05:54 -0600

    added animated sprite and some state machine work

--- SUPERVICTIM/scripts/char_scripts/CharFSM.gd
+++ SUPERVICTIM/scripts/char_scripts/CharFSM.gd
@@ -1,2 +1,43 @@
 extends StateMachine
 
+func _ready():
+	
+	add_state('idle')
+	add_state('walk')
+	add_state('jump')
+	call_deferred('set_state', states.idle)
+	print(states)
+
+func _state_logic(delta):
+	pass
+
+func _get_transition(delta):
+	match state:
+		states.idle:
+			if !parent.check_on_floor():
+				return states.jump
+			elif parent.x_direction !=0:
+				return states.walk
+		states.walk:
+			if !parent.check_on_floor():
+				return states.jump
+			elif parent.x_direction == 0:
+				return states.idle
+		states.jump:
+			if parent.check_on_floor():
+				return states.idle
+
+func _enter_state(old_state, new_state):
+	print(new_state)
+	match new_state:
+		states.idle:
+			parent.SPRITE.animation = 'idle'
+		states.walk:
+			parent.sprite_dir(parent.x_direction)
+			print(parent.x_direction)
+			parent.SPRITE.animation = 'walk'
+		states.jump:
+			parent.SPRITE.animation = 'jump'
+
+func _exit_state(old_state, new_state):
+	pass
--- SUPERVICTIM/scripts/char_scripts/main_char.gd
+++ SUPERVICTIM/scripts/char_scripts/main_char.gd
@@ -3,9 +3,11 @@ var velocity = Vector2(0,0)
 var fall_speed = 5
 var x_direction = 0
 var y_direction = 0
+var sprite_dir = 1
 
 const UP = Vector2(0,-1)
-const GROUND_SPEED = 10000
+const GROUND_SPEED = 5000
+onready var SPRITE = get_node("Sprite")
 
 func _physics_process(delta):
 	get_input(delta)
@@ -21,7 +23,15 @@ func get_input(delta):
 	
 	x_direction = -int(LEFT) + int(RIGHT)
 	y_direction = -int(DOWN) + int(UP)
+	
+	if x_direction != 0:
+		if LEFT:
+			SPRITE.flip_h = true
+		elif RIGHT:
+			SPRITE.flip_h = false
+	
 	move(x_direction, delta)
+	
 
 func apply_gravity(delta):
 	velocity.y += fall_speed * delta
@@ -30,5 +40,11 @@ func apply_gravity(delta):
 func move(direction, delta):
 	velocity.x = direction * GROUND_SPEED * delta
 
+func check_on_floor():
+	return true
 
-
+func sprite_dir(dir):
+	if dir == 1:
+		SPRITE.flip_h = false
+	else:
+		SPRITE.flip_h = true
