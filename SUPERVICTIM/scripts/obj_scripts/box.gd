f7f580748cf4c8897a798bda468dc906028d0a86
newworldmall <84207141+newworldmall@users.noreply.github.com>
2021-05-15 17:14:09 -0600

    stack overflow

--- SUPERVICTIM/scripts/obj_scripts/box.gd
+++ SUPERVICTIM/scripts/obj_scripts/box.gd
@@ -7,19 +7,28 @@ var fall_speed = 10
 const UP = Vector2(0,-1)
 
 
-onready var raycasts = get_node("box_obj/raycasts")
+
 onready var floor_cast = get_node("box_obj/floorcast")
 onready var body = get_node("box_obj")
-	
+
+
+onready var cast_right = get_node("box_obj/right")
+onready var cast_left = get_node("box_obj/left")
+onready var cast_up = get_node("box_obj/up")
+
+var raycasts
 
 func _ready():
 	self.add_to_group("moveable")
+	raycasts = [cast_right, cast_left, cast_up]
 func _process(delta):
 	apply_gravity(delta)
 	position += velocity
 func push_object(dir):
+	
+	check_collider(dir)
+func move(dir):
 	position.x += dir
-	check_collider()
 func check_on_floor():
 	if floor_cast.is_colliding():
 		return true
@@ -32,13 +41,14 @@ func apply_gravity(delta):
 		velocity.y += fall_speed * delta
 	else: 
 		velocity.y = 0
-func check_collider():
-	for i in raycasts.get_children():
-		if i.is_colliding():
-			var obj = i.get_collider()
-			if obj.get_parent() != body:
-				if obj.get_parent().is_in_group("moveable"):
-					print(obj)
+func check_collider(dir):
+	for i in raycasts.size():
+		if raycasts[i].is_colliding():
+			var obj = raycasts[i].get_collider()
+			if obj.get_parent().is_in_group("moveable"):
+				if raycasts[i].name == "left" or raycasts[i].name == "right":
+					obj.get_parent().move(dir)
+					move(dir)
 
 
 
