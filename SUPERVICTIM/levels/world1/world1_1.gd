1493b9e250eefc7bf081a88a1e0417cbc71ca9a5
newworldmall <84207141+newworldmall@users.noreply.github.com>
2021-05-16 19:17:23 -0600

    LEVEL_COMPLETE

--- SUPERVICTIM/levels/world1/world1_1.gd
+++ SUPERVICTIM/levels/world1/world1_1.gd
@@ -6,4 +6,8 @@ func _ready():
 	$collision.visible = false
 	$one_way_collision.visible = false
 	 
-	
+func level_complete():
+	var level_complete = load("res://scenes/level_complete.tscn")
+	level_complete = level_complete.instance()
+	add_child(level_complete)
+	$player.controls_disabled = true
